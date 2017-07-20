fff48933446901c76fef68bd01e932e20a8966b3
jlandesman <jlandesman@users.noreply.github.com>
2017-07-20 19:39:43 -0400

    initial commit

---
+++ r_helpers.R
@@ -0,0 +1,40 @@
+##A series of helper functions for R 
+## source(helper_functions.R)
+
+##load_libraries
+## Function to load my standard libraries
+load_libraries<-function(){
+  library(tidyverse)
+  library(tidyquant)
+  library(xts)
+  library(RODBC)
+  library(RSQLite)
+  library(DBI)
+  library(stringr)
+}
+
+## %like%
+## note: setup is X, pattern = so "test!" %like% test == TRUE, "test" %like% "test!" == FALSE
+`%like%` <- function(x, pattern) str_detect(x, pattern)
+
+
+## %notin%
+## "test" %notint% c("this", "is", "a") == TRUE, "test" %notint% c("this", "is", "a", "test") == FALSE
+`%notin%` <- function(x, y) !(x %in% y)
+
+## sample_for_replication()
+## Use with replicate(n=num_samples, sm(seq_to_sample, size_of_sample))
+#x <- replicate(n=1000, sample_for_replication(5))
+sample_for_replication<-function(seq_to_sample, size_of_sample){
+    sample(children, size=n, replace = FALSE)
+}
+
+## copyTable()
+##   Copy a data frame to clipboard.
+copyTable <- function(x, row.names=FALSE){
+  write.table(x, "clipboard-10000", sep="\t", row.names=row.names, na="")
+}
+
+## readClip()
+## Read from clipboard
+readClip <- function() read.table("clipboard-128", header=T, sep="\t", stringsAsFactors=F)
