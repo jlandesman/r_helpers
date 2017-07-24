44861183d451ac34307bb0626f87db146461de78
jlandesman <jlandesman@users.noreply.github.com>
2017-07-24 11:35:48 -0400

    added get_search_engine_format

--- r_helpers.R
+++ r_helpers.R
@@ -38,3 +38,23 @@ copyTable <- function(x, row.names=FALSE){
 ## readClip()
 ## Read from clipboard
 readClip <- function() read.table("clipboard-128", header=T, sep="\t", stringsAsFactors=F)
+
+## Format strings to search_engine format for regex searching
+## Takes care of parentheses etc
+## input: 'george washington, "george washington"'
+## output: george|washington|george washington
+
+get_search_engine_format<-function(string){
+  regex.escape <- function(string) {
+    gsub("([][{}()+*^${|\\\\?])", "\\\\\\1", string)
+  }
+  
+  string<-gsub(",", "", string)
+  
+  sort.by.length.desc <- function (v) v[order( -nchar(v)) ] 
+  
+  keys <- c(t(read.table(text=string, header=FALSE)))     # Read in the values
+  keys <- sort.by.length.desc(keys)                       # Sort the values
+  pattern = paste(regex.escape(keys), collapse="|")       # Create the pattern
+  return(pattern)
+}
